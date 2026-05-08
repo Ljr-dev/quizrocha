6d5d16f11714caf7e015c37ab5e09069d47f7de6
Leandro <leandrojoserocha@hotmail.com>
2026-05-08 06:03:23 -0300

    Migracao sqlite3 para better-sqlite3

--- routes/auth.js
+++ routes/auth.js
@@ -1,83 +1,168 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+
 const db = require("../database/db");
 
-const SECRET = "quizrocha_secret";
+
+
+/* =========================
+   REGISTER
+========================= */
 
 router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
 
-  const hash = await bcrypt.hash(password, 10);
+  try{
 
-  db.run(
-    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-    [name, email, hash],
-    function (err) {
+    const {
+      name,
+      email,
+      password
+    } = req.body;
 
-      if (err) {
-        return res.status(400).json({
-          error: "Email já cadastrado"
-        });
-      }
 
-      res.json({
-        message: "Usuário criado"
+
+    const existe =
+      db.prepare(`
+        SELECT *
+        FROM users
+        WHERE email = ?
+      `).get(email);
+
+
+
+    if(existe){
+
+      return res.status(400).json({
+        error:"Email já cadastrado"
       });
 
     }
-  );
+
+
+
+    const hash =
+      await bcrypt.hash(password,10);
+
+
+
+    db.prepare(`
+      INSERT INTO users
+      (
+        name,
+        email,
+        password
+      )
+      VALUES (?, ?, ?)
+    `).run(
+      name,
+      email,
+      hash
+    );
+
+
+
+    res.json({
+      message:"Conta criada"
+    });
+
+  }catch(err){
+
+    res.status(500).json({
+      error: err.message
+    });
+
+  }
+
 });
 
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
 
-  db.get(
-    "SELECT * FROM users WHERE email = ?",
-    [email],
 
-    async (err, user) => {
+/* =========================
+   LOGIN
+========================= */
 
-      if (!user) {
-        return res.status(400).json({
-          error: "Usuário não encontrado"
-        });
-      }
+router.post("/login", async (req, res) => {
 
-      const valid = await bcrypt.compare(
+  try{
+
+    const {
+      email,
+      password
+    } = req.body;
+
+
+
+    const user =
+      db.prepare(`
+        SELECT *
+        FROM users
+        WHERE email = ?
+      `).get(email);
+
+
+
+    if(!user){
+
+      return res.status(400).json({
+        error:"Usuário não encontrado"
+      });
+
+    }
+
+
+
+    const valid =
+      await bcrypt.compare(
         password,
         user.password
       );
 
-      if (!valid) {
-        return res.status(400).json({
-          error: "Senha inválida"
-        });
-      }
 
-      const token = jwt.sign(
-  {
-    id: user.id,
-    name: user.name,
-    email: user.email
-  },
-  SECRET
-);
 
-res.json({
-  token,
-  user:{
-    id:user.id,
-    name:user.name,
-    email:user.email
-  }
-});
+    if(!valid){
+
+      return res.status(400).json({
+        error:"Senha inválida"
+      });
 
     }
-  );
+
+
+
+    const token =
+      jwt.sign(
+        {
+          id:user.id
+        },
+        "quizrocha"
+      );
+
+
+
+    res.json({
+      token,
+      user:{
+        id:user.id,
+        name:user.name,
+        email:user.email
+      }
+    });
+
+  }catch(err){
+
+    res.status(500).json({
+      error: err.message
+    });
+
+  }
+
 });
+
+
 
 module.exports = router;
