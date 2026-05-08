24fc3a71a0f4d58bcbeee7e4bf754d3abb99e8af
Leandro <leandrojoserocha@hotmail.com>
2026-05-08 05:59:42 -0300

    Migracao sqlite3 para better-sqlite3

--- database/db.js
+++ database/db.js
@@ -1,72 +1,73 @@
-const sqlite3 = require("sqlite3").verbose();
+const Database =
+  require("better-sqlite3");
 
-const db = new sqlite3.Database(
+const db = new Database(
   "./database/database.sqlite"
 );
 
-db.serialize(() => {
-
-  // USERS
-  db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      email TEXT UNIQUE,
-      password TEXT
-    )
-  `);
 
 
-
-  // QUIZZES
-  db.run(`
-    CREATE TABLE IF NOT EXISTS quizzes (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      user_id INTEGER,
-      title TEXT,
-      slug TEXT UNIQUE,
-      is_public INTEGER DEFAULT 1,
-      password TEXT,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
+// USERS
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT UNIQUE,
+    password TEXT
+  )
+`).run();
 
 
 
-  // QUESTIONS
-  db.run(`
-    CREATE TABLE IF NOT EXISTS questions (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      quiz_id INTEGER,
-      question TEXT
-    )
-  `);
+// QUIZZES
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS quizzes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    title TEXT,
+    slug TEXT UNIQUE,
+    is_public INTEGER DEFAULT 1,
+    password TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`).run();
 
 
 
-  // OPTIONS
-  db.run(`
-    CREATE TABLE IF NOT EXISTS options (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      question_id INTEGER,
-      text TEXT,
-      is_correct INTEGER
-    )
-  `);
+// QUESTIONS
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS questions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    quiz_id INTEGER,
+    question TEXT
+  )
+`).run();
 
 
 
-  // RESULTS
-  db.run(`
-    CREATE TABLE IF NOT EXISTS results (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      quiz_id INTEGER,
-      player_name TEXT,
-      score INTEGER,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
+// OPTIONS
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS options (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question_id INTEGER,
+    text TEXT,
+    is_correct INTEGER
+  )
+`).run();
 
-});
+
+
+// RESULTS
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS results (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    quiz_id INTEGER,
+    player_name TEXT,
+    score INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`).run();
+
+
 
 module.exports = db;
